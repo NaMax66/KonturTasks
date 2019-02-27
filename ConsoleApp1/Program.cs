7f72d6ad668625fe51dee7285fef1431e3c3d5f8
Максим Найдович <max.naid@mail.ru>
2019-02-27 11:17:33 +0500

    сделал третью задачу с временем. Прикольно получилось )))

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,14 +7,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            SecodTask obj = new SecodTask();
-
-            Console.WriteLine("Введите число");
-
-            int i = int.Parse(Console.ReadLine());
-            Console.WriteLine("Перевернутое число:");
-            Console.WriteLine(obj.Reverse(i));
-
+            Console.WriteLine("введите время\n");
+           int time = int.Parse(Console.ReadLine());
+           Console.WriteLine(ThirdTask.GetDegree(time));
         }
 
     }
@@ -52,6 +47,62 @@ namespace ConsoleApp1
             
         }
 
+    }
+    //Expr3.Задано время Н часов(ровно). Вычислить угол в градусах между часовой и минутной стрелками.
+    //Например, 5 часов -> 150 градусов, 20 часов -> 120 градусов.Не использовать циклы.
+   static class ThirdTask
+    {
+        static readonly int oneDegree = 30; //180 градусов это 6 часов на аналоговых часах
+        public static String GetDegree(int time)
+        {
+            int degrees = 0;
+            //todo разделить время time на два возможных случая и добавить исключения в 6 часов и 12 часов.
+            if (time > 12)
+            {
+                time %= 12;
+            }
+            if (time > 6)
+            {
+                degrees = (12 - time) * oneDegree;
+            }
+            else degrees = time * oneDegree;
+
+            return degrees.ToString() + "°";
+        }
+
+
+    }
+
+    //Expr4.Найти количество чисел меньших N, которые имеют простые делители X или Y.
+    class fourth
+    {
+    }
+
+
+
+    //Expr5.Найти количество високосных лет на отрезке[a, b] не используя циклы.
+    class fifth
+    {
+
+    }
+        
+    //Expr6.Посчитать расстояние от точки до прямой, заданной двумя разными точками.
+    class sixth
+    {
+
+    }
+    //Expr7.Найти вектор, параллельный прямой.Перпендикулярный прямой.
+    //Прямая задана коэффициентами уравнения прямой.
+    class seventh
+    {
+
+    }
+
+    //Expr8.Дана прямая L и точка A. Найти точку пересечения прямой L с перпендикулярной ей прямой P, 
+    //проходящей через точку A. Можете считать, что прямая задана либо двумя точками, 
+    //либо коэффициентами уравнения прямой — как вам удобнее.
+    class eighth
+    {
 
     }
 }
